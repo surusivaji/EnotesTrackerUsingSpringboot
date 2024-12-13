bff0ba12c91cc5e263a3c1b06adb6783aa241783
ssiva <ssiva@192.168.0.13>
2024-12-13 21:45:33 +0530

    initial commit

--- Enotes_Tracker/src/main/java/org/siva/enotes/EnotesTrackerApplication.java
+++ Enotes_Tracker/src/main/java/org/siva/enotes/EnotesTrackerApplication.java
@@ -9,15 +9,15 @@ public class EnotesTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnotesTrackerApplication.class, args);
-		 System.out.println("*******************************************");
-	        System.out.println("*                                         *");
-	        System.out.println("*  EEEE  N   N   OOO   TTTTT  EEEE  SSSS  *");
-	        System.out.println("*  E     NN  N  O   O    T    E     S     *");
-	        System.out.println("*  EEEE  N N N  O   O    T    EEEE   SSS  *");
-	        System.out.println("*  E     N  NN  O   O    T    E         S *");
-	        System.out.println("*  EEEE  N   N   OOO     T    EEEE  SSSS  *");
-	        System.out.println("*                                         *");
-	        System.out.println("*******************************************");
+		System.out.println("*******************************************");
+	    System.out.println("*                                         *");
+	    System.out.println("*  EEEE  N   N   OOO   TTTTT  EEEE  SSSS  *");
+	    System.out.println("*  E     NN  N  O   O    T    E     S     *");
+	    System.out.println("*  EEEE  N N N  O   O    T    EEEE   SSS  *");
+	    System.out.println("*  E     N  NN  O   O    T    E         S *");
+	    System.out.println("*  EEEE  N   N   OOO     T    EEEE  SSSS  *");
+	    System.out.println("*                                         *");
+	    System.out.println("*******************************************");
 	}
 
 }
